a0a0a7b627e72ba783c2fe743f31c02da0f1f09b
Pooja jat <patelpoojajat@gmail.com>
2025-10-01 15:13:08 +0530

    updted

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const Navbar = ({ projectName = "Default Todo App" }) => {
+const Navbar = () => {
   return (
     <nav className="navbar bg-dark">
       <div className="container-fluid">
-        <span className="navbar-brand mb-0 h1 text-light">{projectName}</span>
+        <span className="navbar-brand mb-0 h1 text-light">Todo-App</span>
       </div>
     </nav>
   );
