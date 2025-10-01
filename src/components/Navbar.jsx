d0be7218a29c41ff3e1974f53717b2250443a66b
Pooja jat <patelpoojajat@gmail.com>
2025-10-01 15:16:30 +0530

    uddate

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className="navbar bg-dark">
       <div className="container-fluid">
-        <span className="navbar-brand mb-0 h1 text-light">Todo-App</span>
+        <span className="navbar-brand mb-0 h1 text-light text-center">Todo-App</span>
       </div>
     </nav>
   );
